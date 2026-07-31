e7f2abbe512837e9bdbea42f5dbef5ac34bac456
Frank Elsinga <frank@elsinga.de>
2026-08-01 01:56:03 +0200

    Apply suggestion from @CommanderStorm

--- geozero/src/shp/reader.rs
+++ geozero/src/shp/reader.rs
@@ -288,7 +288,7 @@ mod tests {
     use std::io::Cursor;
 
     use super::*;
-    use crate::shp::header::{HEADER_SIZE, ShapeType};
+    use crate::shp::header::{HEADER_SIZE, SIZE_OF_SKIP, FILE_CODE, ShapeType};
     use crate::{ColumnValue, ProcessorSink, PropertyProcessor, error::GeozeroError};
 
     const POLY_SHP: &str = "./tests/data/shp/poly.shp";
